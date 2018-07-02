95bca106f84fbf10acc3b2fe62902d0b53f30b16
ChrisMagnuson <hourglasssand@gmail.com>
2018-07-02 12:20:02 -0400

    Add more troubleshooting SQL

--- TervisCustomizer.psm1
+++ TervisCustomizer.psm1
@@ -156,3 +156,264 @@ SELECT *
   FROM [Integrations].[IntegrationStatus] With (NoLock)
 "@
 }
+
+function Test-CustomizerOracleScheduledOrdersAvailable {
+@"
+SELECT COUNT(*) FROM XXTRVS.XXWIP_DISCRETE_JOBS_MIZER_STG WHERE CREATION_DATE>= SYSDATE - 30/(24*60)
+"@
+}
+
+function Get-CustomizerOracleScheduledOrders {
+@"
+SELECT 
+SALES_ORDER_NUMBER, 
+SALES_ORDER_LINE_NUMBER, 
+MFG_SCHEDULE_NUMBER, 
+WORK_ORDER_STATUS, 
+CREATION_DATE "LAST_UPDATE_DATE" 
+FROM XXTRVS.XXWIP_DISCRETE_JOBS_MIZER_STG 
+WHERE CREATION_DATE>= SYSDATE - 30/(24*60)
+"@
+}
+
+functiuon Invoke-CustomizerOracleScheduledOrdersToCustomizer {
+@"
+USE [customizer_db]
+GO
+
+/****** Object:  StoredProcedure [dbo].[ProcessUpdateReleasedScheduleLineItemsData]    Script Date: 3/16/2018 3:33:05 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[ProcessUpdateReleasedScheduleLineItemsData]
+	@ScheduledOrdersData xml = null
+AS
+
+	UPDATE 
+		[Integrations].IntegrationStatus 
+	SET	
+		[LastSyncDate]=GETDATE()
+	WHERE
+		[IntegrationTypeToken]='SCHEDULEDORDER'
+		
+			
+	BEGIN TRANSACTION	
+	--DECLARE @ScheduledOrdersData xml
+	--SET @ScheduledOrdersData =
+	--'<ns4:Schedules 	xmlns:ns4="http://Tervis.BizTalk.Schedules.Schemas">
+	--	<Schedule 	StatusCode="Released" SalesOrderNumber="6019953" MFGScheduleNumber="04/08-M1-A-097" SalesOrderLineNumber="1"/>
+	--	<Schedule 	StatusCode="Released" SalesOrderNumber="6018015" MFGScheduleNumber="04/08-M1-A-097" SalesOrderLineNumber="3"/>
+	--	<Schedule 	StatusCode="Released" SalesOrderNumber="6017994" MFGScheduleNumber="04/10-A2-A-001" SalesOrderLineNumber="3"/>
+	--	<Schedule 	StatusCode="Released" SalesOrderNumber="6017963" MFGScheduleNumber="04/09-M2-A-008" SalesOrderLineNumber="2"/>
+	--	<Schedule 	StatusCode="Released" SalesOrderNumber="6017310" MFGScheduleNumber="04/08-M1-A-097" SalesOrderLineNumber="2"/>
+	--	<Schedule 	StatusCode="Released" SalesOrderNumber="6016643" MFGScheduleNumber="04/08-M1-A-097" SalesOrderLineNumber="1"/>
+	--	<Schedule 	StatusCode="Released" SalesOrderNumber="6016550" MFGScheduleNumber="04/08-M1-A-097" SalesOrderLineNumber="1"/>
+	--	<Schedule 	StatusCode="Released" SalesOrderNumber="6004422" MFGScheduleNumber="04/08-M1-A-097" SalesOrderLineNumber="1"/>
+	--	<Schedule 	StatusCode="Released" SalesOrderNumber="6004423" MFGScheduleNumber="04/11-A2-A-009" SalesOrderLineNumber="1"/>
+	--	<Schedule 	StatusCode="Released" SalesOrderNumber="6018015" MFGScheduleNumber="04/08-M1-A-099" SalesOrderLineNumber="2"/>
+	--</ns4:Schedules>'
+
+		--;WITH XmlFile (Contents) AS (
+		--SELECT CONVERT (XML, BulkColumn) 
+		--FROM OPENROWSET (BULK 'E:\Mizer Schedule Order Info.xml', SINGLE_BLOB) AS XmlData
+		--)
+		--SELECT @ScheduledOrdersData = Contents
+		--FROM XmlFile
+	
+	--DECLARE @idoc int
+	--EXEC sp_xml_preparedocument @idoc OUTPUT, @ScheduledOrdersData, '<root xmlns:ns4="http://Tervis.BizTalk.Schedules.Schemas" />'
+
+	/* Step-1 : Delete anything with a CreatedDateUTC over 2 months old since the current run time from the 
+		“Integrations.OrdersFromEBS” table */
+	DELETE FROM Integrations.OrdersFromEBS
+	WHERE Integrations.OrdersFromEBS.CreatedDateUTC < DATEADD(month, -2, GETUTCDATE())
+
+	/* Step-1 : End */
+
+	CREATE TABLE #Temp_Orders 
+	(
+		[OrderID]			uniqueidentifier,
+		[OrderDetailID]		uniqueidentifier,
+		[ERPORDERID]		varchar(40),
+		[ERPLINEID]			varchar(255),
+		[SCHEDULENUMBER]	varchar(100),
+		[OrderQuantity]		int
+	)
+	
+	;WITH xmlnamespaces( 'http://Tervis.BizTalk.ScheduledOrders.Schemas' as xx)
+	INSERT INTO #Temp_Orders ([OrderID], [OrderDetailID], [ERPORDERID], [ERPLINEID], [SCHEDULENUMBER], [OrderQuantity])
+	SELECT	DISTINCT
+			[Order].OrderID,
+			[OrderDetail].OrderDetailID,
+			ScheduleOrders.[ERPORDERID],
+			ScheduleOrders.[ERPLINEID],
+			ScheduleOrders.[SCHEDULENUMBER],
+			[OrderDetail].OrderQuantity
+	FROM
+		(
+		
+			SELECT
+				Tab.Col.value('@SalesOrderNumber','VARCHAR(40)') AS [ERPORDERID],
+				Tab.Col.value('@SalesOrderLineNumber', 'VARCHAR(255)') AS [ERPLINEID],
+				Tab.Col.value('@MFGScheduleNumber','VARCHAR(100)') AS [SCHEDULENUMBER],
+				Tab.Col.value('@WorkOrderStatus','VARCHAR(30)') AS [STATUS]
+			FROM   @ScheduledOrdersData.nodes('/xx:ScheduledOrders/ScheduledOrder') Tab(Col)
+		) ScheduleOrders
+		INNER JOIN [Approval].[Order] WITH(nolock)
+		ON (
+			ScheduleOrders.[ERPORDERID]=[Order].[ERPOrderNumber]
+		)
+		INNER JOIN [Approval].[OrderDetail] WITH(nolock)
+		ON (
+			[Order].OrderID = [OrderDetail].OrderID AND
+			ScheduleOrders.[ERPLINEID]=[OrderDetail].ERPOrderLineNumber
+		)
+	WHERE
+		ISNULL(ScheduleOrders.[STATUS],'')='Released' AND ISNULL(ScheduleOrders.[SCHEDULENUMBER],'') <> '' AND Approval.[Order].StatusID=9
+
+	UPDATE	
+		[Approval].[Order]
+	SET		
+		[StatusID]=10
+	FROM	
+		[Approval].[Order] INNER JOIN #Temp_Orders 
+		ON (
+			[Order].OrderID = #Temp_Orders.OrderID
+		)
+
+	UPDATE
+		[Approval].[OrderDetail]
+	SET
+		[StatusID]=10, [ScheduleNumber]=#Temp_Orders.[SCHEDULENUMBER]
+	FROM
+		[Approval].[OrderDetail] INNER JOIN #Temp_Orders
+		ON (
+			[OrderDetail].OrderDetailID = #Temp_Orders.[OrderDetailID]
+		)
+
+	INSERT INTO Integrations.ScheduledOrdersFromEBS (LastSyncedDateTime, Id, InputXMLPayload) VALUES (GETDATE(), NEWID(), @ScheduledOrdersData)
+
+	INSERT INTO Approval.ReviewEventLog 
+	(
+		OrderEventLogID, OrderID, StatusID, Comment, EventTypeID, EventBy, EventDateUTC
+	)
+	SELECT NEWID(), [OrderID], 10, 'Order Scheduled',6,'Integration',GETUTCDATE()
+	FROM
+		#Temp_Orders
+
+	-- CHECK IF PRODUCT IS PRINTING THROUGH DIRECT PRINT
+		
+    ;WITH cte as
+	(
+		SELECT tmpo.[OrderDetailID], tmpo.[SCHEDULENUMBER], tmpo.[OrderQuantity]
+		FROM
+		#Temp_Orders tmpo 
+		join 
+		[Approval].[OrderDetail] od on od.OrderDetailID = tmpo.OrderDetailID 
+		join 
+		Project p on p.ProjectID = od.ProjectID
+		join 
+		Product pr on pr.ProductID = p.ProductID
+		where tmpo.OrderDetailID NOT IN (SELECT OrderDetailID FROM Approval.PackList) 
+		and pr.IsDirectPrint = 0
+	)
+	 INSERT INTO Approval.PackList
+	(
+		PackListID, OrderDetailID, ScheduleNumber, Quantity, CreatedDateUTC
+	)
+	SELECT NEWID(), [OrderDetailID], [SCHEDULENUMBER], [OrderQuantity], GETUTCDATE()
+	FROM cte
+		
+
+ --   INSERT INTO Approval.PackList
+	--(
+	--	PackListID, OrderDetailID, ScheduleNumber, Quantity, CreatedDateUTC
+	--)
+	--SELECT NEWID(), [OrderDetailID], [SCHEDULENUMBER], [OrderQuantity], GETUTCDATE()
+	--FROM
+	--	#Temp_Orders o
+	--WHERE o.OrderDetailID NOT IN (SELECT OrderDetailID FROM Approval.PackList)
+	
+	
+	/* Step-2 : Store scheduled orders sent from EBS into the “Integrations.OrdersFromEBS” table  */
+	DECLARE @CurrentDateUTC datetime = GETUTCDATE()
+	INSERT INTO Integrations.OrdersFromEBS
+	(
+		Id,
+		ERPOrderNumber,
+		ERPOrderLineNumber,
+		ScheduleNumber,
+		CreatedDateUTC
+	)
+	SELECT newid(), [to].ERPORDERID,[to].ERPLINEID,[to].SCHEDULENUMBER,@CurrentDateUTC
+	from #Temp_Orders [to]
+	
+	/* Step-2 : End */
+
+	/* Step-3 : Compare stored scheduling information against all unscheduled orders */
+	
+	EXEC [dbo].[ScheduleExistingOrders]
+	
+	/* End */
+
+COMMIT TRANSACTION;
+GO
+
+
+"@
+}
+
+function Get-CustomizerOracleDiagnosticQueries {
+@"
+SELECT COUNT(*) FROM XXTRVS.XXWIP_DISCRETE_JOBS_MIZER_STG WHERE CREATION_DATE>= SYSDATE - 30/(24*60)
+
+SELECT SALES_ORDER_NUMBER, SALES_ORDER_LINE_NUMBER, MFG_SCHEDULE_NUMBER, WORK_ORDER_STATUS, CREATION_DATE "LAST_UPDATE_DATE" FROM XXTRVS.XXWIP_DISCRETE_JOBS_MIZER_STG WHERE CREATION_DATE>= SYSDATE - 30/(24*60)
+
+SELECT * FROM XXTRVS.XXWIP_DISCRETE_JOBS_MIZER_STG WHERE Sales_Order_Number in ('10694122')
+
+select 
+Sysdate, 
+SYSDATE - 30/(24*60), 
+30/(24*60),
+SYSDATE - 30
+from dual
+
+SELECT SALES_ORDER_NUMBER, SALES_ORDER_LINE_NUMBER, MFG_SCHEDULE_NUMBER, WORK_ORDER_STATUS, CREATION_DATE "LAST_UPDATE_DATE" FROM XXTRVS.XXWIP_DISCRETE_JOBS_MIZER_STG WHERE CREATION_DATE>= SYSDATE - 30/(24*60)
+
+
+
+SELECT * FROM XXTRVS.XXWIP_DISCRETE_JOBS_MIZER_STG
+
+SELECT COUNT(*) FROM XXTRVS.XXWIP_DISCRETE_JOBS_MIZER_STG WHERE CREATION_DATE>= SYSDATE - 30/(24*60)
+
+SELECT SALES_ORDER_NUMBER, SALES_ORDER_LINE_NUMBER, MFG_SCHEDULE_NUMBER, WORK_ORDER_STATUS, CREATION_DATE "LAST_UPDATE_DATE" FROM XXTRVS.XXWIP_DISCRETE_JOBS_MIZER_STG 
+WHERE CREATION_DATE>= SYSDATE - 30/(24*60)
+
+
+SELECT COUNT(*) FROM XXTRVS.XXWIP_DISCRETE_JOBS_MIZER_STG TB1
+INNER JOIN APPS.OE_ORDER_HEADERS_ALL TB2
+ON TB1.SALES_ORDER_NUMBER = TB2.ORDER_NUMBER
+WHERE TB1.LAST_UPDATE_DATE IS NULL AND 
+TB2.SHIPMENT_PRIORITY_CODE IN ('CD-WEB','CD-ITC','RUSH CD-WEB','RUSH CD-ITC','CS-WEB','CS-ITC')
+
+SELECT SALES_ORDER_NUMBER, SALES_ORDER_LINE_NUMBER, MFG_SCHEDULE_NUMBER, WORK_ORDER_STATUS, CREATION_DATE "LAST_UPDATE_DATE", WIP_ENTITY_ID
+FROM XXTRVS.XXWIP_DISCRETE_JOBS_MIZER_STG WHERE SALES_ORDER_NUMBER IN 
+(SELECT SALES_ORDER_NUMBER FROM (
+SELECT DISTINCT TB1.SALES_ORDER_NUMBER FROM XXTRVS.XXWIP_DISCRETE_JOBS_MIZER_STG TB1
+INNER JOIN APPS.OE_ORDER_HEADERS_ALL TB2
+ON TB1.SALES_ORDER_NUMBER = TB2.ORDER_NUMBER
+WHERE TB1.LAST_UPDATE_DATE IS NULL AND TB2.SHIPMENT_PRIORITY_CODE IN ('CD-WEB','CD-ITC','RUSH CD-WEB','RUSH CD-ITC','CS-WEB','CS-ITC')) WHERE ROWNUM < 100)
+
+
+SELECT * FROM XXTRVS.XXWIP_DISCRETE_JOBS_MIZER_STG where last_update_date is null order by WJ_creation_date desc
+"@
+}
+
+function Install-CustomizerPackListGeneration {
+	azurerm
+	https://www.nuget.org/packages/DocumentFormat.OpenXml/ or equivelant
+	
+}
