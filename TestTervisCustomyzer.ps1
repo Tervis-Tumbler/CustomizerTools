73563f292e2a75f590444eacdda3ca269caed6ec
Chris Magnuson <hourglasssand@gmail.com>
2018-12-18 15:11:56 -0500

    Refactor to move packlsit functionality to separate module

--- TestTervisCustomyzer.ps1
+++
@@ -1,6 +0,0 @@
-ipmo -force powershellorm,terviscustomyzer
-$BatchNumber = "20180628-1300"
-Set-CustomyzerModuleEnvironment -Name Epsilon
-$BatchNumber = Get-CustomyzerApprovalPacklistRecentBatch |
-Select-Object -Last 1 -ExpandProperty BatchNumber
-Invoke-CustomyzerPackListDocumentsGenerate -BatchNumber $BatchNumber
