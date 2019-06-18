396dc9776a1ec9b6d30137fdfa3b591eb91a8908
Chris Magnuson <hourglasssand@gmail.com>
2019-06-18 18:28:16 -0400

    Remove old uneeded files

--- Pull project URLS.ps1
+++
@@ -1,8 +0,0 @@
-$ProjectID = "5d564ef7-7ad6-447c-84a9-6e1b348fa197"
-$ProjectID = "4a769801-2991-466f-bcf1-9630b9c38965"
-$ProjectID = "85f174d7-ff45-497e-b2f0-bf18d154a05d"
-New-TervisAdobeScene7VignetteImageURL -ProjectID $ProjectID -Size 16 -FormType DWT
-New-TervisAdobeScene7BaseImageURL -ProjectID $ProjectID -Size 16 -FormType DWT
-
-New-TervisAdobeScene7CustomyzerArtboardImageURL -ProjectID $ProjectID
-New-TervisAdobeScene7VignetteImageURL -ProjectID 4973f23b-9b0d-47ea-9ef2-67439590e079 -Size 16 -FormType DWT
